ffbf6b32245b8194c02cfb35b06b77ac1c0dc17d
Seth Hoenig <seth.a.hoenig@gmail.com>
2015-09-16 14:32:06 -0500

    add setup.sh

---
+++ setup.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+# TweakTool 
+# - Set Caps Lock as Ctrl (in Typing)
+# - Enable extensions Alternateab, Launch new Instance, Systemmonitor (in Extensions)
+# - Enable minimize (in Windows)
+# - Show date (in Top Bar)
+
+# Terminal
+# - Disable bold text 
+# - Disable menu bar in new windows
+# - Disable terminal bell
+# - Change colors to white-on-black
+# - Disable scrollbar
+# - Set Ctl+[ and Ctl+] to move between tabs
+# - Set Ctl+t to create tab
+
+
+# sudo apt-get update -q -y
+# sudo apt-get upgrade -q -y
+
+# sudo apt-get install -q -y \
+#	htop \
+#	tree \
+#	emacs \
+#	git \
+# 	curl
+
+# wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
+# sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+# sudo apt-get update -q -y
+# sudo apt-get install -q -y google-chrome-stable
+
+# Set Chrome to use system borders (right click on border)
+
+# sudo apt-get remove \
+#	shotwell \
+#	empathy \
+#	deja-dup \
+#	spamassassin \
+#	evolution \
+#	cheese \
+#	rhythmbox \
+#	gnome-orca \
+# 	libreoffice*
+
+# sudo apt-get autoremove -q -y
+
+# Go
+# curl -L -o /tmp/go.tar.gz https://storage.googleapis.com/golang/go1.5.1.linux-amd64.tar.gz
+# sudo tar -C /usr/local -xzf /tmp/go.tar.gz
+
+# dotfiles
+# cd $HOME
+# curl -L -o /tmp/dot.zip https://github.com/shoenig/dotfiles2/archive/master.zip
+# unzip /tmp/dot.zip
+# cp -R dotfiles2-master/emacs/.e* $HOME/
+# cp -R dotfiles2-master/bash/.b* $HOME/
+# cp -R dotfiles2-master/git/.g* $HOME/
+
+# ssh-keygen -t rsa -b 4096
+
+# echo COPY THIS KEY TO PLACES like github and eng-git
+# cat ~/.ssh/id_rsa.pub
+
+
+
+
